bca64a5e182cf17000efe9752e40ee1620f00d64
Manish <patelmanish131149198@gmail.com>
2025-02-23 15:15:50 +0530

    [Manish]feat: implement POST /hello/post endpoint to accept firstName and lastName in request body

--- SpringMessagingApp/src/main/java/com/bridgelabz/springmessagingapp/restController/HelloController.java
+++ SpringMessagingApp/src/main/java/com/bridgelabz/springmessagingapp/restController/HelloController.java
@@ -2,6 +2,7 @@ package com.bridgelabz.springmessagingapp.restController;
 
 
 import org.springframework.web.bind.annotation.*;
+import com.bridgelabz.springmessagingapp.dto.UserDTO; 
 
 @RestController
 @RequestMapping("/hello")
@@ -32,6 +33,22 @@ public class HelloController {
     public String getHelloWithQuery(@RequestParam String name) { //UC2
         return "Hello " + name + " from BridgeLabz"; //UC2
     }
+    
+  //UC3
+    
+    @GetMapping("/param/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+    
+    
+    // UC4 - POST request with UserDTO
+    @PostMapping("/post") // UC4
+    public String postHelloWithBody(@RequestBody UserDTO user) { // UC4
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz"; // UC4
+    }
+    
+    
     
 }
 
